786f848b30c18b1a4af54aaa34abd67a64f09d84
alfmat <alfredmathew@outlook.com>
2019-07-04 18:26:19 -0400

    finished activity page
    finished the page that describes cftk activities, added new html files 
    for the get involved section of the website

--- cftk_flask.py
+++ cftk_flask.py
@@ -13,5 +13,25 @@ def my_history():
 def display_execs():
     return render_template('exec_board.html')
 
+@app.route("/our-work")
+def display_work():
+    return render_template('our_work.html')
+
+@app.route("/students")
+def display_work():
+    return render_template('students.html')
+
+@app.route("/sponsors")
+def display_work():
+    return render_template('sponsors.html')
+
+@app.route("/community")
+def display_work():
+    return render_template('community.html')
+
+@app.route("/dancers")
+def display_work():
+    return render_template('dancers.html')
+
 if __name__ == '__main__':
     app.run(debug=1)
