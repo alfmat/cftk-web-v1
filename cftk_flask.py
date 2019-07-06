fc50b5316019b6dbd6bb8678466f176d6b2b8bb9
alfmat <alfredmathew@outlook.com>
2019-07-05 23:40:25 -0400

    more edits
    added new images and modified a few more templates

--- cftk_flask.py
+++ cftk_flask.py
@@ -9,6 +9,10 @@ def hello():
 def my_history():
     return render_template('history.html')
 
+@app.route("/contact")
+def display_contact():
+    return render_template('contact.html')
+
 @app.route("/exec-board")
 def display_execs():
     return render_template('exec_board.html')
@@ -18,19 +22,19 @@ def display_work():
     return render_template('our_work.html')
 
 @app.route("/students")
-def display_work():
+def display_students():
     return render_template('students.html')
 
 @app.route("/sponsors")
-def display_work():
+def display_sponsors():
     return render_template('sponsors.html')
 
 @app.route("/community")
-def display_work():
+def display_community():
     return render_template('community.html')
 
 @app.route("/dancers")
-def display_work():
+def display_dancers():
     return render_template('dancers.html')
 
 if __name__ == '__main__':
